cef01aff07bfe638068d6b0e15619b528c91715d
EDWARDWEE-um <edwardwee11@gmail.com>
2021-01-12 02:11:02 +0800

    Completed Email Reset Function.

--- BeFit/settings.py
+++ BeFit/settings.py
@@ -134,3 +134,10 @@ LOGIN_URL = 'login'
 AUTHENTICATION_BACKENDS = (
     ('django.contrib.auth.backends.ModelBackend'),
 )
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'edwardwee11@gmail.com' # Note to change this to other email
+EMAIL_HOST_PASSWORD = "kqgc btlt vxwv kpvf"
