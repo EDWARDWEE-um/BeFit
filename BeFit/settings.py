36ffa281e5a5c4772d4eac2b802a8cee106968d4
EDWARDWEE-um <edwardwee11@gmail.com>
2021-01-13 09:38:54 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	BeFit/settings.py

--- BeFit/settings.py
+++ BeFit/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'herokuapp',
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
